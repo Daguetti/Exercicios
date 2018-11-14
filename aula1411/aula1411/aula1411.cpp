076c5e6b9783caf376c6ae670dd956d9da632f3a
Daguetti <mathdaguetti@hotmail.com>
2018-11-14 11:34:35 -0200

    struct

---
+++ aula1411/aula1411/aula1411.cpp
@@ -0,0 +1,77 @@
+// aula1411.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/*void troca(float *a, float *b) {
+	float temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+	printf("Numero 1 = %f \nNumero 2 = %f \n", *a, *b);
+}*/
+
+struct Carro {
+	char modelo[30];
+	char marca[20];
+	int anoFab;
+	char cor[20];
+	char placa[8];
+};
+
+void Ex1(){
+	struct Carro opafion;
+	printf("Digite o modelo do carro: \n");
+	gets_s(opafion.modelo);
+	printf("Digite a marca do carro: \n");
+	gets_s(opafion.marca);
+	printf("Digite a cor do carro: \n");
+	gets_s(opafion.cor);
+	printf("Digite a placa do carro: \n");
+	gets_s(opafion.placa);
+	printf("Digite o ano do carro: \n");
+	scanf_s("%i", &opafion.anoFab);
+	//strcpy_s(opafion.modelo, "Ka");
+	printf("Modelo: %s\n", opafion.modelo);
+	//strcpy_s(opafion.marca, "Ford");
+	printf("Marca: %s\n", opafion.marca);
+	//strcpy_s(opafion.cor, "Vermelho");
+	printf("Cor: %s\n", opafion.cor);
+	//strcpy_s(opafion.placa, "abc1234");
+	printf("Placa: %s\n", opafion.placa);
+	//opafion.anoFab = 2000;
+	printf("Ano de Fabricacao: %i\n", opafion.anoFab);
+}
+
+struct Funcionarios {
+	char nome[80];
+	char cpf[11];
+	int diaNasc;
+	int mesNasc;
+	int anoNasc;
+	float salario;
+};
+
+void Ex2(){
+	
+}
+
+int main()
+{
+	/*float *n1, *n2;
+	float p = 1, q = 3;
+	n1 = &p;
+	n2 = &q;
+	troca(n1, n2);*/
+
+	Ex1();
+	//Ex2();
+
+
+	system("pause");
+    return 0;
+}
+
